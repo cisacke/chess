e9da46deef179c2b0c0edf754c31d683a569cd61
Ingrid and Chloe <ingrid.chloe@appacademy.io>
2015-08-04 11:32:54 -0400

    Initialize Pieces class & add #moves.

---
+++ Chess/pieces.rb
@@ -0,0 +1,17 @@
+class Pieces
+
+  def initialize(name, location)
+    @name = name
+    @location = location
+  end
+
+  def moves
+    # if
+    #returns an array of valid positions
+  end
+
+end
+
+
+
+# knight = Piece("knight", [0,1])
